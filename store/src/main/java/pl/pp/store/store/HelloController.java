4e5e8b79ae71d9c5cbe60c3c4b37f1c997d3db45
Pjulax <pawelpo99@gmail.com>
2021-11-12 22:47:20 +0100

    Added scenario for healthcheck with jmeter, unlocked store-service gateway routing, set mapping on hello endpoints

---
+++ store/src/main/java/pl/pp/store/store/HelloController.java
@@ -0,0 +1,17 @@
+package pl.pp.store.store;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity hello(){
+        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+    }
+}
