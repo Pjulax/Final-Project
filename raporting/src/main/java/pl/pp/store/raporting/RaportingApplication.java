3987c2cd000d24ae1af19e071ed7833d921b64ed
Pjulax <pawelpo99@gmail.com>
2021-12-13 23:57:07 +0100

    added raporting module

---
+++ raporting/src/main/java/pl/pp/store/raporting/RaportingApplication.java
@@ -0,0 +1,13 @@
+package pl.pp.store.raporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaportingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaportingApplication.class, args);
+    }
+
+}
---
+++ raporting/src/test/java/pl/pp/store/raporting/RaportingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pp.store.raporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaportingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
