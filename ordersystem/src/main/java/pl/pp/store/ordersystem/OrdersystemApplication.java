58a7eeeaada247b9c233b1e173e63a43040827ff
Pjulax <pawelpo99@gmail.com>
2022-01-19 01:47:22 +0100

    configured Kubernetes built-in service discovery to use

--- ordersystem/src/main/java/pl/pp/store/ordersystem/OrdersystemApplication.java
+++ ordersystem/src/main/java/pl/pp/store/ordersystem/OrdersystemApplication.java
@@ -4,8 +4,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.kubernetes.fabric8.discovery.ConditionalOnKubernetesDiscoveryEnabled;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 @OpenAPIDefinition(info =
 @Info(title = "Order system API", version = "1.0", description = "Documentation Order system API v1.0")
 )
--- store/src/main/java/pl/pp/store/store/StoreApplication.java
+++ store/src/main/java/pl/pp/store/store/StoreApplication.java
@@ -4,10 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 @OpenAPIDefinition(info =
 @Info(title = "Store API", version = "1.0", description = "Documentation Store API v1.0")
 )
